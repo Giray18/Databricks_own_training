f27a9f0189699e5fce52d015154560dc8d2b4d31
Giray18 <Giray18>
2023-08-08 17:28:21 +0000

    Version_2
    Adapted for pipeline

--- Data_Analysis_Transformation_Sample (1).py
+++ Data_Analysis_Transformation_Sample (1).py
@@ -11,45 +11,6 @@
 
 # COMMAND ----------
 
-# MAGIC %scala
-# MAGIC val storageAccount = "training45"
-# MAGIC val container = "premier"
-# MAGIC //val sasKey = "?sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupyx&se=2023-08-04T22:30:50Z&st=2023-08-04T14:30:50Z&spr=https&sig=bqYO8DWF3Q6qWOJojN83uohdFX%2FylEj%2FciYL4EUmHGE%3D"
-# MAGIC val sasKey = "sp=rl&st=2023-08-06T12:17:32Z&se=2023-08-11T20:17:32Z&spr=https&sv=2022-11-02&sr=c&sig=EHz1FFk1BZVQgOh0yq3Yp2Pj6FRyWsJmsEAXXsv1FcM%3D"
-# MAGIC val mountPoint = s"/mnt/mymountpointname"
-# MAGIC  
-# MAGIC  
-# MAGIC try {
-# MAGIC   dbutils.fs.unmount(s"$mountPoint") // Use this to unmount as needed
-# MAGIC } catch {
-# MAGIC   case ioe: java.rmi.RemoteException => println(s"$mountPoint already unmounted")
-# MAGIC }
-# MAGIC  
-# MAGIC  
-# MAGIC val sourceString = s"wasbs://$container@$storageAccount.blob.core.windows.net/"
-# MAGIC val confKey = s"fs.azure.sas.$container.$storageAccount.blob.core.windows.net"
-# MAGIC  
-# MAGIC  
-# MAGIC  
-# MAGIC try {
-# MAGIC   dbutils.fs.mount(
-# MAGIC     source = sourceString,
-# MAGIC     mountPoint = mountPoint,
-# MAGIC     extraConfigs = Map(confKey -> sasKey)
-# MAGIC   )
-# MAGIC }
-# MAGIC catch {
-# MAGIC   case e: Exception =>
-# MAGIC     println(s"*** ERROR: Unable to mount $mountPoint. Run previous cells to unmount first")
-# MAGIC }
-# MAGIC  
-# MAGIC
-# MAGIC //fs ls /mnt/mymountpointname/
-# MAGIC  
-# MAGIC /*This will list all the files located at the container*/
-
-# COMMAND ----------
-
 storageAccount = "training45"
 container = "premier"
 sasKey = "sp=rl&st=2023-08-06T12:17:32Z&se=2023-08-11T20:17:32Z&spr=https&sv=2022-11-02&sr=c&sig=EHz1FFk1BZVQgOh0yq3Yp2Pj6FRyWsJmsEAXXsv1FcM%3D"
@@ -123,11 +84,6 @@ dbutils.fs.mount(
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC SELECT * FROM Players;
-
-# COMMAND ----------
-
 # MAGIC %python
 # MAGIC from datetime import datetime, timedelta
 # MAGIC from pyspark.sql.functions import year
@@ -147,17 +103,6 @@ dbutils.fs.mount(
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC -- DESCRIBE EXTENDED players
-# MAGIC DESCRIBE HISTORY players_only   -- sadece tablolarda calisiyor HISTORY
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC DESCRIBE EXTENDED players
-
-# COMMAND ----------
-
 # MAGIC %python
 # MAGIC
 # MAGIC df1 = spark.read.json("/mnt/mymountpointname/epl_2022_2023_07_02_2023.json",multiLine=True)
@@ -171,87 +116,78 @@ dbutils.fs.mount(
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC SELECT * FROM EPL_RECORDS
+# %python
+# from pyspark.sql.functions import explode
+# from pyspark.sql.functions import concat,col
+# from functools import reduce
+# from pyspark.sql import DataFrame
+# # from pyspark.sql.DataFrame import *
+
+# column_names = list(df1.columns)
+
+# # starting_lineups
+# # df5 = df1.select(explode("0.team1_startings"))
+
+# player_startings = []
+# for i in column_names:
+#     df5 = df1.select(explode(f"{i}.team1_startings")).distinct()
+#     df6 = df1.select(explode(f"{i}.team2_startings")).distinct()
+#     player_startings.append(df5)
+#     player_startings.append(df6)
+
+# df_output = functools.reduce(DataFrame.union, player_startings)
+
+# players_unique = df_output.distinct()
+
+
+# display(players_unique)
+
+# players_unique.select("col").alias("Born_Year")
+
+# players_unique.write.format("delta").option("path", "/player_names").saveAsTable("Unique_Player_Names")
 
 # COMMAND ----------
 
-# MAGIC %python
-# MAGIC from pyspark.sql.functions import explode
-# MAGIC from pyspark.sql.functions import concat,col
-# MAGIC from functools import reduce
-# MAGIC from pyspark.sql import DataFrame
-# MAGIC # from pyspark.sql.DataFrame import *
-# MAGIC
-# MAGIC column_names = list(df1.columns)
-# MAGIC
-# MAGIC # starting_lineups
-# MAGIC # df5 = df1.select(explode("0.team1_startings"))
-# MAGIC
-# MAGIC player_startings = []
-# MAGIC for i in column_names:
-# MAGIC     df5 = df1.select(explode(f"{i}.team1_startings")).distinct()
-# MAGIC     df6 = df1.select(explode(f"{i}.team2_startings")).distinct()
-# MAGIC     player_startings.append(df5)
-# MAGIC     player_startings.append(df6)
-# MAGIC
-# MAGIC df_output = functools.reduce(DataFrame.union, player_startings)
-# MAGIC
-# MAGIC players_unique = df_output.distinct()
-# MAGIC
-# MAGIC
-# MAGIC display(players_unique)
-# MAGIC
-# MAGIC players_unique.select("col").alias("Born_Year")
-# MAGIC
-# MAGIC players_unique.write.format("delta").option("path", "/player_names").saveAsTable("Unique_Player_Names")
+# %python
+# from pyspark.sql.functions import dense_rank
+# from pyspark.sql.window import Window
+# from pyspark.sql.functions import row_number
+# from pyspark.sql import SparkSession, functions, types, SQLContext
+# from pyspark.sql.types import *
+# import pyspark.sql.functions as F
+# import json
+# import pandas as pd
+# from pyspark.sql.functions import udf, col
+# import re
+# from datetime import datetime, timedelta
+
+
+# df7 = spark.read.table("Unique_Player_Names").withColumnRenamed("col","Player_name")
+
+# df8 = spark.read.table("players_only")
+
+# df10 = df7.join(df8,df7.Player_name == df8.player_name,"inner") #Sadece oynayan oyuncular
+
+# df.createOrReplaceTempView("df11")
+
+# df.createOrReplaceTempView("df10")
+
+# df11 = spark.sql("SELECT team,count(*) FROM df11 GROUP BY team")
+
+# df10 = spark.sql("SELECT * FROM df10")
+
+# df10 = df10.join(df11,df10.team == df11.team,"left")
+
+# display(df11)
+
+# spark.sql("SELECT * FROM df11").show()
 
 # COMMAND ----------
 
-# MAGIC %python
-# MAGIC from pyspark.sql.functions import dense_rank
-# MAGIC from pyspark.sql.window import Window
-# MAGIC from pyspark.sql.functions import row_number
-# MAGIC from pyspark.sql import SparkSession, functions, types, SQLContext
-# MAGIC from pyspark.sql.types import *
-# MAGIC import pyspark.sql.functions as F
-# MAGIC import json
-# MAGIC import pandas as pd
-# MAGIC from pyspark.sql.functions import udf, col
-# MAGIC import re
-# MAGIC from datetime import datetime, timedelta
-# MAGIC
-# MAGIC
-# MAGIC df7 = spark.read.table("Unique_Player_Names").withColumnRenamed("col","Player_name")
-# MAGIC
-# MAGIC df8 = spark.read.table("players_only")
-# MAGIC
-# MAGIC df10 = df7.join(df8,df7.Player_name == df8.player_name,"inner") #Sadece oynayan oyuncular
-# MAGIC
-# MAGIC df.createOrReplaceTempView("df11")
-# MAGIC
-# MAGIC df.createOrReplaceTempView("df10")
-# MAGIC
-# MAGIC df11 = spark.sql("SELECT team,count(*) FROM df11 GROUP BY team")
-# MAGIC
-# MAGIC df10 = spark.sql("SELECT * FROM df10")
-# MAGIC
-# MAGIC df10 = df10.join(df11,df10.team == df11.team,"left")
-# MAGIC
-# MAGIC display(df11)
-# MAGIC
-# MAGIC spark.sql("SELECT * FROM df11").show()
+# %sql
 
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC
-# MAGIC CREATE OR REPLACE TABLE TEAMS_WITH_STARTING_LINEUPS_2
-# MAGIC USING DELTA
-# MAGIC LOCATION '/data/sales/'
-# MAGIC AS SELECT team,count(*) AS sayim ,RANK() OVER (ORDER BY count(1) DESC) Sira FROM df11 GROUP BY team;
-# MAGIC
-
-# COMMAND ----------
-
+# CREATE OR REPLACE TABLE TEAMS_WITH_STARTING_LINEUPS_2
+# USING DELTA
+# LOCATION '/data/sales/'
+# AS SELECT team,count(*) AS sayim ,RANK() OVER (ORDER BY count(1) DESC) Sira FROM df11 GROUP BY team;
 
